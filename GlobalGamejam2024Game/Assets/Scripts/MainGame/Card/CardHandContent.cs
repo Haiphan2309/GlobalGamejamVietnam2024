d0ae3995c109c34bcbea00ddb5be7c9d22a5c79b
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-01-27 14:23:06 +0700

    feat: card hand Content

---
+++ GlobalGamejam2024Game/Assets/Scripts/MainGame/Card/CardHandContent.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardHandContent : MonoBehaviour
+{
+    [SerializeField] private LayoutGroup _layoutGroup;
+    
+    
+    public void AddCardToHand(CardView card)
+    {
+        Transform transform1 = card.transform;
+        transform1.SetParent(transform);
+        transform1.localScale = Vector3.one;
+    }
+    
+    public void RemoveCardFromHand(CardView card)
+    {
+        card.transform.SetParent(null);
+    }
+    
+    public void RemoveCardFromHand(int index)
+    {
+        transform.GetChild(index).SetParent(null);
+    }
+    
+    
+    public void ClearHand()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    
+    
+    
+    
+
+
+
+}
