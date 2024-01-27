d03da49c4f7049dcb73dd1fcad771ad29393f9fe
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-01-27 15:44:13 +0700

    import: dialog system

---
+++ GlobalGamejam2024Game/Assets/Scripts/MainGame/Dialog/DialogInputManager.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityUtilities;
+
+
+public class DialogInputManager : SingletonMonoBehaviour<DialogInputManager>
+{
+    
+    public bool GetSubmitPressed()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+    
+    
+    public bool RegisterSubmitPressed()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+}
