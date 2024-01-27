75e048a1c48bb19bfd98203048e4f2389484a079
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-01-27 21:58:25 +0700

    feat: tween for card move

--- GlobalGamejam2024Game/Assets/Scripts/MainGame/Card/CardInfoController.cs
+++ GlobalGamejam2024Game/Assets/Scripts/MainGame/Card/CardInfoController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CardInfoController : MonoBehaviour
@@ -7,6 +8,15 @@ public class CardInfoController : MonoBehaviour
     [SerializeField] private CardInfoView _cardInfoView;
     [SerializeField] private float _delayBeforeShowingCardInfo = 2;
 
+    [SerializeField] private RectTransform _cardInfoTransform;
+    [SerializeField] private RectTransform _showTransform;
+    [SerializeField] private RectTransform _hideTransform;
+    [SerializeField] private Ease _moveEase = Ease.OutCubic;
+    [SerializeField] private float _moveDuration = 0.25f;
+    
+    private Tween _moveTween;
+    
+    
     private Coroutine delay;
     public void ShowCardInfo(SO_Card cardSo)
     {
@@ -17,7 +27,12 @@ public class CardInfoController : MonoBehaviour
     {
         StopCoroutine(delay);
         
-        _cardInfoView.gameObject.SetActive(false);
+        _moveTween.Kill();
+        _moveTween = _cardInfoTransform.transform.DOMove(_hideTransform.position, _moveDuration).SetEase(_moveEase)
+            .OnComplete(() =>
+            {
+                _cardInfoView.gameObject.SetActive(false);
+            });
     }
     
     IEnumerator ShowCardInfoCoroutine(SO_Card cardSo)
@@ -25,6 +40,12 @@ public class CardInfoController : MonoBehaviour
         yield return new WaitForSeconds(_delayBeforeShowingCardInfo);
         _cardInfoView.gameObject.SetActive(true);
         _cardInfoView.LoadInfo(cardSo);
+        
+        _cardInfoTransform.transform.position = _hideTransform.position;
+        
+        _moveTween.Kill();
+        _moveTween = _cardInfoTransform.transform.DOMove(_showTransform.position, _moveDuration).SetEase(_moveEase);
+        
     }
     
     
